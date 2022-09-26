4435921120d596adf4d5a19f1f41848047329add
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-09-26 19:52:21 -0400

    Worked on sign in system

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBg-Ywxz8NEjihDL_6xFRTm2ki4ZGKKBqc",
@@ -17,3 +18,21 @@ provider.setCustomParameters({prompt: 'select_account'});
 
 export const auth = getAuth();
 export const signInWithGooglePopup = signInWithPopup(auth, provider);
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        
+        try {
+            await setDoc(userDocRef, {displayName, email, createdAt});
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+     
+        return userDocRef;
+    }
+}
