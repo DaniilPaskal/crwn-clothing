4b80b9117d6615df36cbfd0976a68af23ed5f621
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-05 19:49:33 -0400

    Created sign in form

--- src/routes/home/authentication/authentication.component.jsx
+++ src/routes/home/authentication/authentication.component.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { getRedirectResult } from "firebase/auth";
 import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from "../../../utils/firebase/firebase.utils";
 import SignUpForm from "../../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../../components/sign-in-form/sign-in-form.component";
 
 const Authentication = () => {
     /*
@@ -20,8 +21,8 @@ const Authentication = () => {
 
     return (
         <div>
-            <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <h1>Sign In Page</h1>
+            <SignInForm/>
             <SignUpForm/>
         </div>
     );
