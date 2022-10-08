85d162a0676b3922c3ddc00d10bc68cb67e6fffe
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-07 22:17:14 -0400

    Added authentication state change listener

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -21,6 +21,7 @@ const SignInForm = () => {
 
     const signInWithGoogle = async () => {
         const { user } = await signInWithGooglePopup();
+        setCurrentUser(user);
         const userDocRef = await createUserDocumentFromAuth(user);
     }
 
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import { onAuthStateChangedListener, signOutUser } from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
     currentUser: null,
@@ -8,6 +9,11 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {})
+        return unsubscribe;
+    }, []);
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 }
