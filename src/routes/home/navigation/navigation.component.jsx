9b7cd5b5ce796b7a837de11ebd97c5e6242d6433
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-07 22:21:21 -0400

    Removed redundant calls to setCurrentUser

--- src/routes/home/navigation/navigation.component.jsx
+++ src/routes/home/navigation/navigation.component.jsx
@@ -6,11 +6,7 @@ import { signOutUser } from "../../../utils/firebase/firebase.utils";
 import './navigation.styles.scss';
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  }
+  const { currentUser} = useContext(UserContext);
 
   return (
     <Fragment>
@@ -22,7 +18,7 @@ const Navigation = () => {
           <div className='nav-links-container'>
               <Link className='nav-link' to='/shop'>SHOP</Link>
               {currentUser ? (
-                  <span className='nav-link' onClick={signOutHandler}>SIGN OUT</span>
+                  <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
                 ) : (
                   <Link className='nav-link' to='/auth'>SIGN IN</Link>
                 )}
